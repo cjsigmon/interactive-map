7f2c5be20e86d59dc3993e5a5b5ea4e7b6894c31
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-11-30 11:46:32 -0500

    make triggers for all sections, refactor

--- js/scrollMap.js
+++ js/scrollMap.js
@@ -2,24 +2,54 @@
 var controller = new ScrollMagic.Controller();
 var direction;
 
-// build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 400})
-                .addTo(controller)
-                .addIndicators() // add indicators (requires plugin)
-                .on("update", function (e) {
-                    direction = (e.target.controller().info("scrollDirection"));
-                    $("#scrollDirection").text(e.target.controller().info("scrollDirection"));
-                })
-                .on("enter leave", function (e) {
-                    $("#state").text(e.type == "enter" ? "inside" : "outside");
-                })
-                .on("start end", function (e) {
-                    console.log(direction);
-                    $("#lastHit").text(e.type == "start" ? "top" : "bottom");
-                    if (e.type != "start" && direction == "FORWARD") {
-                        nextPlace();
-                    }
-                })
-                .on("progress", function (e) {
-                    $("#progress").text(e.progress.toFixed(3));
-                });
+$(function () { // wait for document ready
+    // init
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave',
+            duration: "20%" // this works just fine with duration 0 as well
+            // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+            // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+        }
+    });
+
+    // get all slides
+    var slides = document.querySelectorAll("section.panel");
+
+    // create scene for every slide
+    for (var i=0; i<slides.length; i++) {
+        new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+            .setPin(slides[i], {pushFollowers: false})
+            .addTo(controller)
+            .addIndicators() 
+            .on("update", function (e) {
+                direction = (e.target.controller().info("scrollDirection"));
+            })
+            .on("start end", function (e) {
+                console.log(direction);
+                if (e.type != "start" && direction == "FORWARD") {
+                    nextPlace();
+                }
+            });
+
+    }
+});
+
+// for (let i = 0; i < worldWonders.length; i++) {
+//     var triggerId = "#trigger"+i;
+//     var scene = new ScrollMagic.Scene({triggerElement: triggerId, duration: 400})
+//     .addTo(controller)
+//     .addIndicators() // add indicators (requires plugin)
+//     .on("update", function (e) {
+//         direction = (e.target.controller().info("scrollDirection"));
+//     })
+//     .on("start end", function (e) {
+//         console.log(direction);
+//         if (e.type != "start" && direction == "FORWARD") {
+//             nextPlace();
+//         }
+//     })
+// }
+ 
