20a4f157339aaed401befa6fafedbb0ec395e191
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-11-30 11:21:11 -0500

    scroll forward on first location changes map

--- js/index.js
+++ js/index.js
@@ -2,45 +2,33 @@ const moveBtn = document.getElementById("moveBtn");
 const API_KEY = 'pk.eyJ1IjoiY2FsZWJqc2lnbW9uIiwiYSI6ImNscGh0Y2RtaDA1NDAycXFzMmI3ZDRuamkifQ.yzxnVlFnXxb0jjMzWlv_EQ';
 var locationIndex = 0;
 const markerSet = new Set();
-
-const worldWondersCoordinates = [
-    // Coordinates for various world wonders
-    // Format: [longitude, latitude]
-    [-72.544962, -13.163068], // Machu Picchu, Peru
-    [116.407396, 39.9042],    // Great Wall of China, China
-    [29.9773, 31.1325],       // Pyramids of Giza, Egypt
-    [77.7100, 20.9890],       // Taj Mahal, India
-    [12.4924, 41.8902],       // Colosseum, Italy
-    [34.1356, -0.7103]        // Mount Kilimanjaro, Tanzania
-    // Add more wonders as needed
-  ];
-  const worldWonders = [
-    {
-      name: 'Machu Picchu, Peru',
-      coordinates: [-72.544962, -13.163068]
-    },
-    {
-      name: 'Great Wall of China, China',
-      coordinates: [116.407396, 39.9042]
-    },
-    {
-      name: 'Pyramids of Giza, Egypt',
-      coordinates: [29.9773, 31.1325]
-    },
-    {
-      name: 'Taj Mahal, India',
-      coordinates: [77.7100, 20.9890]
-    },
-    {
-      name: 'Colosseum, Italy',
-      coordinates: [12.4924, 41.8902]
-    },
-    {
-      name: 'Mount Kilimanjaro, Tanzania',
-      coordinates: [34.1356, -0.7103]
-    }
-    // Add more wonders as needed
-  ];
+const worldWonders = [
+  {
+    name: 'Machu Picchu, Peru',
+    coordinates: [-72.544962, -13.163068]
+  },
+  {
+    name: 'Great Wall of China, China',
+    coordinates: [116.407396, 39.9042]
+  },
+  {
+    name: 'Pyramids of Giza, Egypt',
+    coordinates: [29.9773, 31.1325]
+  },
+  {
+    name: 'Taj Mahal, India',
+    coordinates: [77.7100, 20.9890]
+  },
+  {
+    name: 'Colosseum, Italy',
+    coordinates: [12.4924, 41.8902]
+  },
+  {
+    name: 'Mount Kilimanjaro, Tanzania',
+    coordinates: [34.1356, -0.7103]
+  }
+  // Add more wonders as needed
+];
 markerSet.add(worldWonders[locationIndex].coordinates)
 
 function offsetLeft(coordinates) {
@@ -48,6 +36,7 @@ function offsetLeft(coordinates) {
     coordsCopy[0] += 1.8; // Adjust the longitude value to offset the center to the left
     return coordsCopy;
 }
+
 mapboxgl.accessToken = API_KEY;
 centerOffset = offsetLeft(worldWonders[locationIndex].coordinates);
 console.log(centerOffset);
@@ -58,8 +47,6 @@ const map = new mapboxgl.Map({
     zoom: 7, // starting zoom
     interactive: false,
 });
-
-
 // Create a new marker.
 const marker = new mapboxgl.Marker()
     .setLngLat(worldWonders[locationIndex].coordinates)
@@ -69,33 +56,35 @@ const marker = new mapboxgl.Marker()
           // Handle click events on the points
           console.log('Clicked:', worldWonders[locationIndex].name);
         });
-moveBtn.addEventListener("click", function() {
-    if (locationIndex < worldWonders.length - 1) {
-        locationIndex++;
-    } else {
-        locationIndex = 0;
-    }
-    let cameraOffset = offsetLeft(worldWonders[locationIndex].coordinates);
-    map.flyTo({
-        center: cameraOffset,
-        zoom: 7,
-        speed: 1,
-        curve: 1,
-        easing(t) {
-        return t;
-        }
-    });
-    map.on('moveend', () => {
-        if (!markerSet.has(worldWonders[locationIndex].coordinates)) {
-            markerSet.add(worldWonders[locationIndex].coordinates);
-            const marker = new mapboxgl.Marker()
-                .setLngLat(worldWonders[locationIndex].coordinates)
-                .addTo(map)
-                .getElement()
-                    .addEventListener('click', () => {
-                    // Handle click events on the points
-                    console.log('Clicked:', worldWonders[locationIndex].name);
-                });
-        }
-    });
-});
+
+function nextPlace() {
+  if (locationIndex < worldWonders.length - 1) {
+      locationIndex++;
+  } else {
+      locationIndex = 0;
+  }
+  let cameraOffset = offsetLeft(worldWonders[locationIndex].coordinates);
+  map.flyTo({
+      center: cameraOffset,
+      zoom: 7,
+      speed: 1,
+      curve: 1,
+      easing(t) {
+      return t;
+      }
+  });
+  map.on('moveend', () => {
+      if (!markerSet.has(worldWonders[locationIndex].coordinates)) {
+          markerSet.add(worldWonders[locationIndex].coordinates);
+          const marker = new mapboxgl.Marker()
+              .setLngLat(worldWonders[locationIndex].coordinates)
+              .addTo(map)
+              .getElement()
+                  .addEventListener('click', () => {
+                  // Handle click events on the points
+                  console.log('Clicked:', worldWonders[locationIndex].name);
+              });
+      }
+  });
+}     
+moveBtn.addEventListener("click", nextPlace);
--- js/scrollMap.js
+++ js/scrollMap.js
@@ -1,18 +1,24 @@
 // init controller
 var controller = new ScrollMagic.Controller();
+var direction;
 
 // build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 200})
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 400})
                 .addTo(controller)
                 .addIndicators() // add indicators (requires plugin)
                 .on("update", function (e) {
+                    direction = (e.target.controller().info("scrollDirection"));
                     $("#scrollDirection").text(e.target.controller().info("scrollDirection"));
                 })
                 .on("enter leave", function (e) {
                     $("#state").text(e.type == "enter" ? "inside" : "outside");
                 })
                 .on("start end", function (e) {
+                    console.log(direction);
                     $("#lastHit").text(e.type == "start" ? "top" : "bottom");
+                    if (e.type != "start" && direction == "FORWARD") {
+                        nextPlace();
+                    }
                 })
                 .on("progress", function (e) {
                     $("#progress").text(e.progress.toFixed(3));
